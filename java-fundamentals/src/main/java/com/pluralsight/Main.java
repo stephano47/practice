253ffa721ed3ca3869335ac8877953af41b2b29b
stephano47 <stephanomelv@gmail.com>
2024-11-23 02:21:07 -0500

    Switch statement readability - JAVA

--- java-fundamentals/src/main/java/com/pluralsight/Main.java
+++ java-fundamentals/src/main/java/com/pluralsight/Main.java
@@ -1,6 +1,17 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // switch cases
+           System.out.println("Choose a number");
+           int testSwitch = scanner.nextInt();
+        switch (testSwitch){
+            case 0 -> System.out.println(0); // Arrow allows me to make code readable as it includes the break method
+            case 1 -> System.out.println(1);
+        }
     }
 }
