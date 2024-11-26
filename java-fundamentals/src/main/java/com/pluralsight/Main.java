8649455be4943c87c5e3002b299c18337b35d654
stephano47 <stephanomelv@gmail.com>
2024-11-26 12:57:43 -0500

    redoing workbooks (WB1- Exercise 1 complete)

--- java-fundamentals/src/main/java/com/pluralsight/Main.java
+++ java-fundamentals/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,5 @@
 package com.pluralsight;
-
 import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -12,6 +10,47 @@ public class Main {
         switch (testSwitch){
             case 0 -> System.out.println(0); // Arrow allows me to make code readable as it includes the break method
             case 1 -> System.out.println(1);
-        }
+
+        }codeWars();
     }
-}
+
+    public static  void codeWars(){
+                int wholeNumber = 42;
+
+                switch (wholeNumber){
+                    case 1:
+                        wholeNumber = -1;
+                        System.out.println(wholeNumber);
+                        break;
+                    case 2:
+                        wholeNumber = -2;
+                        System.out.println(wholeNumber);
+                        break;
+                    case 3:
+                        wholeNumber = -3;
+                        System.out.println(wholeNumber);
+                        break;
+                    case 4:
+                        wholeNumber = -4;
+                        System.out.println(wholeNumber);
+                        break;
+                    case 5:
+                        wholeNumber = -5;
+                        System.out.println(wholeNumber);
+                        break;
+                    case 6:
+                        wholeNumber = -6;
+                        System.out.println(wholeNumber);
+                        break;
+                    case 42 :
+                        wholeNumber = -42;
+                        System.out.println(wholeNumber);
+                        break;
+
+                }
+
+            }
+
+
+        }
+
