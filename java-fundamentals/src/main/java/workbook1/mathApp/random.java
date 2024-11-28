60d1b0d9b9df33581a231db15e5d470541434dfb
stephano47 <stephanomelv@gmail.com>
2024-11-27 20:47:40 -0500

    Math Apllication Exercise complete

---
+++ java-fundamentals/src/main/java/workbook1/mathApp/random.java
@@ -0,0 +1,8 @@
+package workbook1.mathApp;
+
+public class random {
+    public static void main(String []args){
+        int randomNum = (int)(Math.random() * 2); // allows it only print out whole numbers
+        System.out.println(randomNum);
+    }
+}
