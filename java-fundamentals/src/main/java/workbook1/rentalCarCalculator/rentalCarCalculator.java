5ee30ec2c0d25f040efde9771a5803176655426f
stephano47 <stephanomelv@gmail.com>
2024-11-29 19:06:29 -0500

    Car Rental Exercise complete

--- java-fundamentals/src/main/java/workbook1/rentalCarCalculator/rentalCarCalculator.java
+++ java-fundamentals/src/main/java/workbook1/rentalCarCalculator/rentalCarCalculator.java
@@ -45,61 +45,171 @@ public class rentalCarCalculator {
         }
 
         // current age
-        System.out.println("How old are you?" + " Note there is a surcharge fee if you are under 25 years old");
+        System.out.println("How old are you?" + " Note: There is a surcharge fee if you are under 25 years old");
         int age = scanner.nextInt();
         // display all info
         double basicCarRental = 29.99;
         double tollTagPrice = 3.95;
         double gpsPrice = 2.95;
         double roadsidePrice = 3.95;
-        if (age >= 25 && !tollTag){
-            if (!gps){
-                if (!roadSideAssist){
-                    // this will be a scenario if user is 25 and up and chooses no optional costs
-                    System.out.println("Here is your overview of your planned rental!");
-                    System.out.println("------------------------------------------------");
-                    System.out.println("Pickup Date: "+ pickupDate);
-                    System.out.println("Days of Rental: "+ days);
-                    System.out.println("Current Age: "+ age);// will have to put into if statements for specific outcomes
-                    System.out.println("Daily Car Rental:" + "$"+basicCarRental);
-                    System.out.println("Optional Costs: N/A");
-                    System.out.println("Underage Driver Surcharge N/A");
-                    System.out.println("Total: $" + String.format("%.2f", days * basicCarRental));
-                }
-            }
-        } else if (age < 25 &&!tollTag) {
-            if (!gps){
-                if (!roadSideAssist){
-                    // this will be a scenario if user is 25 and below and chooses no optional costs
-                    System.out.println("Here is your overview of your planned rental!");
-                    System.out.println("------------------------------------------------");
-                    System.out.println("Pickup Date: "+ pickupDate);
-                    System.out.println("Days of Rental: "+ days);
-                    System.out.println("Current Age: "+ age);// will have to put into if statements for specific outcomes
-                    System.out.println("Daily Car Rental:" + "$"+basicCarRental);
-                    System.out.println("Optional Costs: N/A");
-                    System.out.println("Underage Driver Surcharge: Applied");
-                    System.out.println("Total: $" + String.format("%.2f", days * basicCarRental * 1.3));
-                }
-            }
-        } else if (age >= 25 &&tollTag){
-            if (!gps){
-                if (!roadSideAssist){
-                    System.out.println("Here is your overview of your planned rental!");
-                    System.out.println("------------------------------------------------");
-                    System.out.println("Pickup Date: "+ pickupDate);
-                    System.out.println("Days of Rental: "+ days);
-                    System.out.println("Current Age: "+ age);// will have to put into if statements for specific outcomes
-                    System.out.println("Daily Car Rental:" + "$"+basicCarRental);
-                    System.out.println("Optional Costs: Toll Tag: $" + tollTagPrice + " Per Day");
-                    System.out.println("Underage Driver Surcharge: Applied");
-                    System.out.println("Total: $" + String.format("%.2f", days * basicCarRental * tollTagPrice));
-                }
-            }
+        if (age >= 25 && !tollTag && !gps && !roadSideAssist){
+            // this will be a scenario if user is 25 and up and chooses no optional costs
+            System.out.println("Here is your overview of your planned rental!");
+            System.out.println("------------------------------------------------");
+            System.out.println("Pickup Date: "+ pickupDate);
+            System.out.println("Days of Rental: "+ days);
+            System.out.println("Current Age: "+ age);// will have to put into if statements for specific outcomes
+            System.out.println("Daily Car Rental:" + "$"+basicCarRental);
+            System.out.println("Optional Costs: N/A");
+            System.out.println("Underage Driver Surcharge N/A");
+            System.out.println("Total: $" + String.format("%.2f", days * basicCarRental));
+        } else if (age < 25 && !tollTag && !gps && !roadSideAssist) {
+            // this will be a scenario if user is 25 and below and chooses no optional costs
+            System.out.println("Here is your overview of your planned rental!");
+            System.out.println("------------------------------------------------");
+            System.out.println("Pickup Date: "+ pickupDate);
+            System.out.println("Days of Rental: "+ days);
+            System.out.println("Current Age: "+ age);// will have to put into if statements for specific outcomes
+            System.out.println("Daily Car Rental:" + "$"+basicCarRental);
+            System.out.println("Optional Costs: N/A");
+            System.out.println("Underage Driver Surcharge: Applied");
+            System.out.println("Total: $" + String.format("%.2f", days * basicCarRental * 1.3));
+        }// if user wants toll tag
+        else if (age >= 25 && tollTag && !gps && !roadSideAssist) {
+            System.out.println("Here is your overview of your planned rental!");
+            System.out.println("------------------------------------------------");
+            System.out.println("Pickup Date: "+ pickupDate);
+            System.out.println("Days of Rental: "+ days);
+            System.out.println("Current Age: "+ age);// will have to put into if statements for specific outcomes
+            System.out.println("Daily Car Rental:" + "$"+basicCarRental);
+            System.out.println("Optional Costs: Toll Tag: $" + tollTagPrice + " Per Day");
+            System.out.println("Underage Driver Surcharge: N/A");
+            System.out.println("Total: $" + String.format("%.2f", days * basicCarRental * tollTagPrice));
+        } else if (age < 25 && tollTag && !gps && !roadSideAssist) {
+            System.out.println("Here is your overview of your planned rental!");
+            System.out.println("------------------------------------------------");
+            System.out.println("Pickup Date: "+ pickupDate);
+            System.out.println("Days of Rental: "+ days);
+            System.out.println("Current Age: "+ age);// will have to put into if statements for specific outcomes
+            System.out.println("Daily Car Rental:" + "$"+basicCarRental);
+            System.out.println("Optional Costs: N/A");
+            System.out.println("Underage Driver Surcharge: Applied");
+            System.out.println("Total: $" + String.format("%.2f", days * basicCarRental * tollTagPrice * 1.3));
+        // easier to read
+        }else if (age >= 25 && tollTag && gps && !roadSideAssist){
+            System.out.println("Here is your overview of your planned rental!");
+            System.out.println("------------------------------------------------");
+            System.out.println("Pickup Date: "+ pickupDate);
+            System.out.println("Days of Rental: "+ days);
+            System.out.println("Current Age: "+ age);// will have to put into if statements for specific outcomes
+            System.out.println("Daily Car Rental:" + "$"+basicCarRental);
+            System.out.println("Optional Costs: Toll Tag:"+ "$"+tollTagPrice+" per day \n"+"GPS: $"+ gpsPrice+" per day");
+            System.out.println("Underage Driver Surcharge: N/A");
+            System.out.println("Total: $" + String.format("%.2f", days * basicCarRental * tollTagPrice ));
+        }else if (age < 25 && tollTag && gps && !roadSideAssist){
+            System.out.println("Here is your overview of your planned rental!");
+            System.out.println("------------------------------------------------");
+            System.out.println("Pickup Date: "+ pickupDate);
+            System.out.println("Days of Rental: "+ days);
+            System.out.println("Current Age: "+ age);// will have to put into if statements for specific outcomes
+            System.out.println("Daily Car Rental:" + "$"+basicCarRental);
+            System.out.println("Optional Costs: Toll Tag:"+ "$"+tollTagPrice+" per day \n"+"GPS: $"+ gpsPrice+" per day");
+            System.out.println("Underage Driver Surcharge: Applied");
+            System.out.println("Total: $" + String.format("%.2f", days * basicCarRental * tollTagPrice * 1.3));
+        }// If age >= 25 and only Road Side Assistance is selected
+        else if (age >= 25 && !tollTag && !gps && roadSideAssist) {
+            System.out.println("Here is your overview of your planned rental!");
+            System.out.println("------------------------------------------------");
+            System.out.println("Pickup Date: " + pickupDate);
+            System.out.println("Days of Rental: " + days);
+            System.out.println("Current Age: " + age);
+            System.out.println("Daily Car Rental: $" + basicCarRental);
+            System.out.println("Optional Costs: Road Side Assistance: $" + roadsidePrice + " per day");
+            System.out.println("Underage Driver Surcharge: N/A");
+            System.out.println("Total: $" + String.format("%.2f", days * (basicCarRental + roadsidePrice)));
         }
+
+// If age < 25 and only Road Side Assistance is selected
+        else if (age < 25 && !tollTag && !gps && roadSideAssist) {
+            System.out.println("Here is your overview of your planned rental!");
+            System.out.println("------------------------------------------------");
+            System.out.println("Pickup Date: " + pickupDate);
+            System.out.println("Days of Rental: " + days);
+            System.out.println("Current Age: " + age);
+            System.out.println("Daily Car Rental: $" + basicCarRental);
+            System.out.println("Optional Costs: Road Side Assistance: $" + roadsidePrice + " per day");
+            System.out.println("Underage Driver Surcharge: Applied");
+            System.out.println("Total: $" + String.format("%.2f", days * (basicCarRental + roadsidePrice) * 1.3));
+        }
+
+// If age >= 25 and selects all options (Toll Tag, GPS, and Road Side Assistance)
+        else if (age >= 25 && tollTag && gps && roadSideAssist) {
+            System.out.println("Here is your overview of your planned rental!");
+            System.out.println("------------------------------------------------");
+            System.out.println("Pickup Date: " + pickupDate);
+            System.out.println("Days of Rental: " + days);
+            System.out.println("Current Age: " + age);
+            System.out.println("Daily Car Rental: $" + basicCarRental);
+            System.out.println("Optional Costs: ");
+            System.out.println(" - Toll Tag: $" + tollTagPrice + " per day");
+            System.out.println(" - GPS: $" + gpsPrice + " per day");
+            System.out.println(" - Road Side Assistance: $" + roadsidePrice + " per day");
+            System.out.println("Underage Driver Surcharge: N/A");
+            System.out.println("Total: $" + String.format("%.2f", days * (basicCarRental + tollTagPrice + gpsPrice + roadsidePrice)));
+        }
+
+// If age < 25 and selects all options (Toll Tag, GPS, and Road Side Assistance)
+        else if (age < 25 && tollTag && gps && roadSideAssist) {
+            System.out.println("Here is your overview of your planned rental!");
+            System.out.println("------------------------------------------------");
+            System.out.println("Pickup Date: " + pickupDate);
+            System.out.println("Days of Rental: " + days);
+            System.out.println("Current Age: " + age);
+            System.out.println("Daily Car Rental: $" + basicCarRental);
+            System.out.println("Optional Costs: ");
+            System.out.println(" - Toll Tag: $" + tollTagPrice + " per day");
+            System.out.println(" - GPS: $" + gpsPrice + " per day");
+            System.out.println(" - Road Side Assistance: $" + roadsidePrice + " per day");
+            System.out.println("Underage Driver Surcharge: Applied");
+            System.out.println("Total: $" + String.format("%.2f", days * (basicCarRental + tollTagPrice + gpsPrice + roadsidePrice) * 1.3));
+        }
+
+// If age >= 25 and selects GPS and Road Side Assistance (No Toll Tag)
+        else if (age >= 25 && !tollTag && gps && roadSideAssist) {
+            System.out.println("Here is your overview of your planned rental!");
+            System.out.println("------------------------------------------------");
+            System.out.println("Pickup Date: " + pickupDate);
+            System.out.println("Days of Rental: " + days);
+            System.out.println("Current Age: " + age);
+            System.out.println("Daily Car Rental: $" + basicCarRental);
+            System.out.println("Optional Costs: ");
+            System.out.println(" - GPS: $" + gpsPrice + " per day");
+            System.out.println(" - Road Side Assistance: $" + roadsidePrice + " per day");
+            System.out.println("Underage Driver Surcharge: N/A");
+            System.out.println("Total: $" + String.format("%.2f", days * (basicCarRental + gpsPrice + roadsidePrice)));
+        }
+
+// If age < 25 and selects GPS and Road Side Assistance (No Toll Tag)
+        else if (age < 25 && !tollTag && gps && roadSideAssist) {
+            System.out.println("Here is your overview of your planned rental!");
+            System.out.println("------------------------------------------------");
+            System.out.println("Pickup Date: " + pickupDate);
+            System.out.println("Days of Rental: " + days);
+            System.out.println("Current Age: " + age);
+            System.out.println("Daily Car Rental: $" + basicCarRental);
+            System.out.println("Optional Costs: ");
+            System.out.println(" - GPS: $" + gpsPrice + " per day");
+            System.out.println(" - Road Side Assistance: $" + roadsidePrice + " per day");
+            System.out.println("Underage Driver Surcharge: Applied");
+            System.out.println("Total: $" + String.format("%.2f", days * (basicCarRental + gpsPrice + roadsidePrice) * 1.3));
+        }
+
 
 
     }
 
 
-}
+    }
+
+
+
