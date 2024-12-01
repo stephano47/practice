a6d61628d6c65a0d543ce6ce25435b0e6964f79e
stephano47 <stephanomelv@gmail.com>
2024-12-01 00:59:29 -0500

    Full Name Generator Complete

--- java-fundamentals/src/main/java/workbook2/FullNameApplication/FullNameApplication.java
+++ java-fundamentals/src/main/java/workbook2/FullNameApplication/FullNameApplication.java
@@ -1,4 +1,41 @@
 package workbook2.FullNameApplication;
-
+import java.util.*;
 public class FullNameApplication {
+    public static void main(String []args){
+        String FirstName = PromptForString("First Name: ");
+
+        String MiddleName = PromptForString("Middle Name: ");
+        String LastName = PromptForString("Last Name: ");
+        String Suffix = PromptForString("Suffix ");
+
+        String FullName;
+
+        FullName = FirstName + MiddleName + LastName + Suffix;
+        if(MiddleName.isBlank()){
+            if(Suffix.isBlank()){
+                FullName = FirstName + " " + LastName;
+            }
+            else{
+                FullName = FirstName + " " + LastName + ", " + Suffix;
+            }
+        }
+        else{
+            if(Suffix.isBlank()){
+                FullName = FirstName + " " + MiddleName + " " + LastName;
+            }
+            else
+            {
+                FullName = FirstName + " " + MiddleName + " " + LastName + ", " + Suffix;
+            }
+        }
+
+        System.out.println("Hello " + FullName);
+    }
+    public static String PromptForString(String prompt){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(prompt);
+        return scanner.nextLine();
+
+
+    }
 }
