c130fdeead741762cd1a71b792a27d86d9b4defc
stephano47 <stephanomelv@gmail.com>
2024-12-08 04:21:10 -0500

    Looping Exersices created (WhileLoop Complete)

---
+++ java-fundamentals/src/main/java/workbook2/LoopingExcersises/WhileLoop.java
@@ -0,0 +1,11 @@
+package workbook2.LoopingExcersises;
+
+public class WhileLoop {
+    public static void main(String []args){
+        int i = 1;
+         while (i <= 5){ // it starts with zero, instead i can start it as one
+             System.out.println("I love Java");
+             i++;
+         }
+    }
+}
