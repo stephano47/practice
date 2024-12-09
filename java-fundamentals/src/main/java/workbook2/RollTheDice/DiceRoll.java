9b133311f211ad0898249b2c5ebd516d8c68df0e
stephano47 <stephanomelv@gmail.com>
2024-12-09 00:09:09 -0500

    Dice Roll wip

--- java-fundamentals/src/main/java/workbook2/RollTheDice/DiceRoll.java
+++ java-fundamentals/src/main/java/workbook2/RollTheDice/DiceRoll.java
@@ -3,14 +3,30 @@ package workbook2.RollTheDice;
 import workbook1.mathApp.random;
 
 public class DiceRoll {
-    public static void main(String[]args){
+    public static void main(String[]args) throws InterruptedException {
+        // roll loop
         Roll roll = new Roll();
-       int roll1 = roll.diceRoll();
+       int i = 1;
+       int roll1 = roll.diceRoll(); // calls from the roll class using the math random
        int roll2 = roll.diceRoll();
+       int sum = roll1 + roll2;
+     do {
+         System.out.println("----------------\n" + "Dice Roll #" + i);
+         System.out.println("Dice 1: "+roll1);
+         System.out.println("Dice 2: "+roll2);
+         System.out.println("Sum: "+sum);
+         System.out.println("----------------");
+         i++;
+     }while (i <= 100);
+     Thread.sleep(100);
+     System.out.println("");
+       int sum2 = 0, sum4 =0, sum6 = 0, sum7 =0;
 
-
-        System.out.println(roll1);
-        System.out.println(roll2);
+     if (sum == 2){
+        sum2++;
+     }else if (sum == 4){
+         
+     }
     }
 
 }
